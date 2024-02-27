991e5bbda2b60612ac6a045722be2928e8c7208c
Zach Lor <zacharylor2020@gmail.com>
2024-02-27 13:08:02 -0800

    Removed client code from server

--- PythonCode/Server_Client_Tests/server.py
+++ PythonCode/Server_Client_Tests/server.py
@@ -16,35 +16,6 @@ UDP_IP = '127.0.0.1'
 global UDP_PORT
 UDP_PORT = 2345
 BUFF_SIZE = 1024
-
-def clientFunc():
-  s = socket.socket()  # Create a socket object
-  port = 8080  # Reserve a port for your service every new transfer wants a new port or you must wait.
-
-  s.connect(('localhost', port))
-  x = 0
-
-  st = str(x)
-  byt = st.encode()
-  s.send(byt)
-
-  # send message for hundred times
-  while x < 100:
-    st = str(x)
-    byt = st.encode()
-    s.send(byt)
-    print(x)
-
-    data = s.recv(1024)
-    if data:
-      print(data)
-      x += 1
-      break
-    else:
-      print('no data received')
-
-  print('closing')
-  s.close()
   
 def distanceData():
   distance = board.getDistance()
